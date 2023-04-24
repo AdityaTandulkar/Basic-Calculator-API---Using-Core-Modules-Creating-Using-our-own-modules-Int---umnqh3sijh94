9a84a55edddc784a85b62f87c2e45733a02463d7
AdityaTandulkar <adityatandulkar20@gmail.com>
2023-04-24 14:18:02 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,19 +1,175 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const port = 3000
-app.use(express.urlencoded());
+const port = 3000;
 
+function isUnderflow(parameter) {
+  if (parameter < -1000000) {
+    return true;
+  }
+  return false;
+}
+
+function isOverflow(parameter) {
+  if (parameter > 1000000) {
+    return true;
+  }
+  return false;
+}
+
+app.use(express.urlencoded());
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
-
-
-app.use(bodyParser.urlencoded({ extended: false }))
-
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 // your code goes here
 
+//custom middleware
+app.use((req, res, next) => {
+    const {num1, num2} = req.body;
+    if(typeof num1 === "string" || typeof num2 === "string"){
+        res.status(400).send({
+            status : "error",
+            message : "Invalid data types"
+        });
+    }
+    else if(isOverflow(num1) || isOverflow(num2)){
+        res.status(400).send({
+            status : "error",
+            message : "Overflow"
+        });
+    }
+    else if(isUnderflow(num1) || isUnderflow(num2)){
+        res.status(400).send({
+            status : "error",
+            message : "Underflow"
+        })
+    }
+    else{
+        next();
+    }
+});
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/add", (req, res) => {
+    const {num1, num2} = req.body;
+    const result = num1 + num2;
+
+    if(isUnderflow(result)){
+        res.status(400).send({
+            status : "error",
+            message : "Underflow"
+        })
+    }
+    else if(isOverflow(result)){
+        res.status(400).send({
+            status : "error",
+            message : "Overflow"
+        })
+    }
+    else{
+        res.send({
+            status : "success",
+            message : "the sum of given two numbers",
+            sum : result
+        })
+    }
+});
+
+
+
+app.post("/sub", (req, res) => {
+    const {num1, num2} = req.body;
+    const result = num1 - num2;
+
+    if(isUnderflow(result)){
+        res.status(400).send({
+            status : "error",
+            message : "Underflow"
+        })
+    }
+    else if(isOverflow(result)){
+        res.status(400).send({
+            status : "error",
+            message : "Overflow"
+        })
+    }
+    else{
+        res.send({
+            status : "success",
+            message : "the difference of given two numbers",
+            sum : result
+        })
+    }
+});
+
+
+
+app.post("/multiply", (req, res) => {
+    const {num1, num2} = req.body;
+    const result = num1 * num2;
+
+    if(isUnderflow(result)){
+        res.status(400).send({
+            status : "error",
+            message : "Underflow"
+        })
+    }
+    else if(isOverflow(result)){
+        res.status(400).send({
+            status : "error",
+            message : "Overflow"
+        })
+    }
+    else{
+        res.send({
+            status : "success",
+            message : "the product of given numbers",
+            sum : result
+        })
+    }
+});
+
+
+
+app.post("/divide", (req, res) => {
+    const {num1, num2} = req.body;
+    const result = num1 / num2;
+
+    if(num2 === 0){
+        res.status(400).send({
+            status : "error",
+            message : "Cannot divide by zero"
+        })
+    }
+    else if(isUnderflow(result)){
+        res.status(400).send({
+            status : "error",
+            message : "Underflow"
+        })
+    }
+    else if(isOverflow(result)){
+        res.status(400).send({
+            status : "error",
+            message : "Overflow"
+        })
+    }
+    else{
+        res.send({
+            status : "success",
+            message : "the division of given numbers",
+            sum : result
+        })
+    }
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
